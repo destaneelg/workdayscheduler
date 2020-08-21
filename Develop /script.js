5d929ef39544e7a50663ec45c8d9ab20cbf4523d
Destanee Glenn <destanee.glenn@gmail.com>
2020-08-20 23:48:42 -0500

    Changed button formatting, attempted to fix colors,  maybe its too late..probably didnt work

---
+++ Develop /script.js
@@ -0,0 +1,16 @@
+var events = $("col-6");
+
+//function to cycle through the array and change colors 
+events.forEach(function(){
+    if (currentDay < moment.js){
+        this.style.color = ".past"
+    }
+    if (currentDay > moment.js){
+        this.style.color = ".future"
+    }
+    else {
+        this.style.color = ".present"
+    }
+});
+
+  document.getElementById$("currentDay").innerHTML = moment();
