7a941219965c735f049dd8e6e329c505280c2fae
Destanee Glenn <destanee.glenn@gmail.com>
2020-12-27 09:11:51 -0600

    updating all files to get color changing to work

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 var currentDay = moment().format("MMMM Do YYYY");
 $("#currentDay").append(currentDay);
 
-$(".saveBtn").on("click", function() {
+var time = moment().hour();
+
+$(".button").on("click", function() {
     var hour = $(this).parent().attr("id");
-    var activity = $("input").val().trim();
+    var activity = $(this).siblings(".textContent").val();
     localStorage.setItem(hour, activity);
 });
-$("#9am").children("input").val(localStorage.getItem("9am"));
-$("#10am").children("input").val(localStorage.getItem("10am"));
-$("#11am").children("input").val(localStorage.getItem("11am"));
-$("#12pm").children("input").val(localStorage.getItem("12pm"));
-$("#1pm").children("input").val(localStorage.getItem("1pm"));
-$("#2pm").children("input").val(localStorage.getItem("2pm"));
-$("#3pm").children("input").val(localStorage.getItem("3pm"));
-$("#4pm").children("input").val(localStorage.getItem("4pm"));
-$("#5pm").children("input").val(localStorage.getItem("5pm"));
 
+$(".hour").each(function() {
+    var hour = parseInt($(this).attr("data-hour"));
+    if(time < hour) {
+        $(this).addClass("future")
+    } else if (hour === time) {
+        $(this).addClass("present");
+    } else {
+        $(this).addClass("past")
+    }
+});
+
+$("#9am.textContent").val(localStorage.getItem("9am"));
+$("#10am.textContent").val(localStorage.getItem("10am"));
+$("#11am.textContent").val(localStorage.getItem("11am"));
+$("#12pm.textContent").val(localStorage.getItem("12pm"));
+$("#1pm.textContent").val(localStorage.getItem("1pm"));
+$("#2pm.textContent").val(localStorage.getItem("2pm"));
+$("#3pm.textContent").val(localStorage.getItem("3pm"));
+$("#4pm.textContent").val(localStorage.getItem("4pm"));
+$("#5pm.textContent").val(localStorage.getItem("5pm"));
